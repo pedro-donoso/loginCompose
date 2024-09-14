9c055f21ef25d60a1bf282c922acbc0c6f863987
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-14 17:48:54 -0300

    first commit

---
+++ app/src/main/java/com/example/logincompose/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.logincompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.logincompose.ui.theme.LoginComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                    .padding(vertical = 30.dp, horizontal = 20.dp),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginComposeTheme {
+        Greeting("Android")
+    }
+}
