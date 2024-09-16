d52155bf5883b8820f43a16923f3dd84798ef9ac
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-16 00:08:59 -0300

    agrego TextField

--- app/src/main/java/com/example/logincompose/MainActivity.kt
+++ app/src/main/java/com/example/logincompose/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,11 +28,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
+@Composable
+fun LoginForm() {
+    Screen {
+        Column {
+            TextField(value = "", onValueChange = {})
+            TextField(value = "", onValueChange = {})
+            Button(
+                onClick = {
+                
+            }
+            ) {
+            }
+        }
+    }
+}
+
 @Composable
 fun Screen(content: @Composable () -> Unit) {
     LoginComposeTheme {
         Surface(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
                 .padding(vertical = 30.dp, horizontal = 20.dp),
             color = MaterialTheme.colorScheme.background,
             content = content
@@ -37,13 +58,7 @@ fun Screen(content: @Composable () -> Unit) {
     }
 }
 
-@Preview
-@Composable
-fun LoginForm() {
-    Screen {
-        Text(text = "Login")
-    }
-}
+
 
 
 
