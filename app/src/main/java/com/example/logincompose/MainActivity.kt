abf79dfb32c1c3d168d881bf96678567e53222dc
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-16 00:15:29 -0300

    centro login

--- app/src/main/java/com/example/logincompose/MainActivity.kt
+++ app/src/main/java/com/example/logincompose/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,14 +37,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LoginForm() {
     Screen {
-        Column {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
+        ) {
             TextField(value = "", onValueChange = {})
             TextField(value = "", onValueChange = {})
-            Button(
-                onClick = {
-                
-            }
-            ) {
+            Button(onClick = {}) {
+                Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null )
+                Text(text = "Login")
             }
         }
     }
