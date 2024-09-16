ff32c503d6a016bb29bbf2c09acf7f15551bc9f1
pedro-donoso <pedro.donoso.r@usach.cl>
2024-09-16 01:06:47 -0300

    Agrego icono password

--- app/src/main/java/com/example/logincompose/MainActivity.kt
+++ app/src/main/java/com/example/logincompose/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -23,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.logincompose.ui.theme.LoginComposeTheme
@@ -70,7 +73,13 @@ fun PasswordField(value: String, onValueChange: (String) -> Unit) {
     OutlinedTextField(
         value = value,
         onValueChange = onValueChange,
-        label = { Text(text = "Password") }
+        label = { Text(text = "Password") },
+        visualTransformation = PasswordVisualTransformation(),
+        trailingIcon = {
+            IconToggleButton(checked = false, onCheckedChange = {}) {
+                Icon(imageVector = Icons.Default.Lock, contentDescription = null)
+            }
+        }
     )
 }
 
